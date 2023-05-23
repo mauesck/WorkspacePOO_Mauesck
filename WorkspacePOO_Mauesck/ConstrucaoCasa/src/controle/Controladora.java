9d6e394affdaa51df1b24635c018551c41d08101
mauesck <mauesckt@gmail.com.br>
2023-05-22 21:49:00 -0300

    Atividade 3 OT11

--- WorkspacePOO_Mauesck/ConstrucaoCasa/src/controle/Controladora.java
+++ WorkspacePOO_Mauesck/ConstrucaoCasa/src/controle/Controladora.java
@@ -8,115 +8,138 @@ import modelo.*;
 
 public class Controladora {
 
-	//OT8.2
-	//obejo casa do tipo Casa sendo isntânciado vazio.
+	// OT8.2
+	// obejo casa do tipo Casa sendo isntânciado vazio.
 	private Casa casa = null;
 
 	public void exibeMenu() {
+		String verificaConstrucaoCasa = "";
 		int opcao;
 		do {
 			opcao = EntradaSaida.solicitaOpcao();
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 0:
-				
-				//OT9.4
-				//objeto casa recebendo o construtor Casa().
+
+				// OT9.4
+				// objeto casa recebendo o construtor Casa().
 				this.casa = new Casa();
-				
-				//Variáveis recebendo o retorno de métodos.
-				String descricao = EntradaSaida.solicitaDescricao("casa",0);
+
+				// Variáveis recebendo o retorno de métodos.
+				String descricao = EntradaSaida.solicitaDescricao("casa", 0);
 				String cor = EntradaSaida.solicitaCor();
 				int qtdePortas = EntradaSaida.solicitaQtdeAberturas("portas");
 				int qtdeJanelas = EntradaSaida.solicitaQtdeAberturas("janelas");
-				
-				//OT9.6
-				//*1* > Declaração de um ArrayList do tipo Aberturas, que receberá cada uma das portas cadastradas.
+
+				// OT9.6
+				// *1* > Declaração de um ArrayList do tipo Aberturas, que receberá cada uma das
+				// portas cadastradas.
 				ArrayList<Aberturas> listaDePortas = new ArrayList<Aberturas>();
-				//Este for é necessário para inserir a quantidade de descrições e estados para a quantidade de portas.
-				for(int i=0; i<qtdePortas; i++) {
-					//Objeto porta do tipo Porta de construtor Porta().
+				// Este for é necessário para inserir a quantidade de descrições e estados para
+				// a quantidade de portas.
+				for (int i = 0; i < qtdePortas; i++) {
+					// Objeto porta do tipo Porta de construtor Porta().
 					Porta porta = new Porta();
-					//Objeto porta acessando o método setDescricao() da classe Aberturas que está capturando o retorno do método solicitaDescricao() da classe EntradaSaida.
-					porta.setDescricao(EntradaSaida.solicitaDescricao("porta", (i+1)));
+					// Objeto porta acessando o método setDescricao() da classe Aberturas que está
+					// capturando o retorno do método solicitaDescricao() da classe EntradaSaida.
+					porta.setDescricao(EntradaSaida.solicitaDescricao("porta", (i + 1)));
 					porta.setEstado(EntradaSaida.solicitaEstado("porta"));
 					listaDePortas.add(porta);
 				}
-				
-				//*2* > Os mesmos eventos de *1* ocorrerão aqui.
+
+				// *2* > Os mesmos eventos de *1* ocorrerão aqui.
 				ArrayList<Aberturas> listaDeJanelas = new ArrayList<Aberturas>();
-				for(int i=0; i<qtdeJanelas; i++) {
+				for (int i = 0; i < qtdeJanelas; i++) {
 					Janela janela = new Janela();
-					janela.setDescricao(EntradaSaida.solicitaDescricao("janela", (i+1)));
+					janela.setDescricao(EntradaSaida.solicitaDescricao("janela", (i + 1)));
 					janela.setEstado(EntradaSaida.solicitaEstado("janela"));
 					listaDeJanelas.add(janela);
 				}
-				
-				//OT9.8
-				//Objeto casa acessando o metodo constroiCasa().
-				this.casa.constroiCasa(descricao,cor,listaDePortas,listaDeJanelas);
-				
-				//OT9.10
-				//Mostra valores dos métodos.
-				System.out.print("Descrição da casa: "+ casa.getDescricao()+"\n");
-				System.out.print("Cor da casa: "+casa.getCor()+"\n");
-				
-				for(Aberturas porta : casa.getListaDePortas()) {
-					System.out.print("Descricao da porta: "+porta.getDescricao()+"\n");
-					System.out.print("Estado da porta: "+porta.getEstado()+"\n");
+
+				// OT9.8
+				// Objeto casa acessando o metodo constroiCasa().
+				this.casa.constroiCasa(descricao, cor, listaDePortas, listaDeJanelas);
+
+				// OT9.10
+				// Mostra valores dos métodos.
+				System.out.print("Descrição da casa: " + casa.getDescricao() + "\n");
+				System.out.print("Cor da casa: " + casa.getCor() + "\n");
+
+				for (Aberturas porta : casa.getListaDePortas()) {
+					System.out.print("Descricao da porta: " + porta.getDescricao() + "\n");
+					System.out.print("Estado da porta: " + porta.getEstado() + "\n");
 				}
-				
-				for(Aberturas janela : casa.getListaDeJanelas()) {
-					System.out.print("Descricao da janela: "+janela.getDescricao()+"\n");
-					System.out.print("Estado da janela: "+janela.getEstado()+"\n");
+
+				for (Aberturas janela : casa.getListaDeJanelas()) {
+					System.out.print("Descricao da janela: " + janela.getDescricao() + "\n");
+					System.out.print("Estado da janela: " + janela.getEstado() + "\n");
 				}
-				
-				JOptionPane.showMessageDialog(null, "Construir casa");
-			break;
-			
+
+				verificaConstrucaoCasa = "Contrução bem sucedida!";
+
+				JOptionPane.showMessageDialog(null, verificaConstrucaoCasa);
+				break;
+
 			case 1:
-				//OT10.1
-				//Variável recebendo o retorno do médoto.
-				String tipoAbertura = EntradaSaida.solicitaTipoAbertura();
-				
-				//OT10.2
-				ArrayList<Aberturas> listaDeAberturas = new ArrayList<Aberturas>();
-				
-				//Validando o retorno do método solicitaTipoAbertura().
-				if(tipoAbertura.equals("porta")) {
-					/*Objeto casa acessando getListaDePortas() enviando o retorno 
-					a var ListaDeAberturas do tipo ArrayList*/ 
-					listaDeAberturas = this.casa.getListaDePortas();
-				}else {
-					listaDeAberturas = this.casa.getListaDeJanelas();
+				// PERGUNTAR PQ FAZER ESSA VALIDAÇÃO FORA DO CASE DA ERRO.
+				if (verificaConstrucaoCasa.equals("Contrução bem sucedida!")) {
+
+					// OT10.1
+					// Variável recebendo o retorno do médoto.
+					String tipoAbertura = EntradaSaida.solicitaTipoAbertura();
+
+					// OT10.2
+					ArrayList<Aberturas> listaDeAberturas = new ArrayList<Aberturas>();
+
+					// Validando o retorno do método solicitaTipoAbertura().
+					if (tipoAbertura.equals("porta")) {
+						/*
+						 * Objeto casa acessando getListaDePortas() enviando o retorno a var
+						 * ListaDeAberturas do tipo ArrayList
+						 */
+						listaDeAberturas = this.casa.getListaDePortas();
+					} else {
+						listaDeAberturas = this.casa.getListaDeJanelas();
+					}
+
+					// OT10.3
+					// Var posicao recebendo o retorno do método que tem como parametro a var
+					// listaDeAberturas.
+					int posicao = EntradaSaida.solicitaAberturaMover(listaDeAberturas);
+					int novoEstado = 0;
+
+					// OT10.5
+					if (posicao != -1) {
+						novoEstado = EntradaSaida.solicitaEstado(tipoAbertura);
+						Aberturas abertura = this.casa.retornaAbertura(posicao, tipoAbertura);
+						this.casa.moverAbertura(abertura, novoEstado);
+						System.out.print("Novo estado da " + tipoAbertura + ": " + abertura.getEstado());
+					} else {
+						EntradaSaida.exibeMsgEncerraPrograma();
+					}
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Não há casa construida!");
 				}
-				
-				//OT10.3
-				//Var posicao recebendo o retorno do método que tem como parametro a var listaDeAberturas.
-				int posicao = EntradaSaida.solicitaAberturaMover(listaDeAberturas);
-				int novoEstado=0;
-				
-				//OT10.5
-				if(posicao!=-1) {
-					novoEstado = EntradaSaida.solicitaEstado(tipoAbertura);
-					Aberturas abertura = this.casa.retornaAbertura(posicao, tipoAbertura);
-					this.casa.moverAbertura(abertura, novoEstado);
-					System.out.print("Novo estado da "+tipoAbertura+": "+abertura.getEstado());
-				}else {
-					EntradaSaida.exibeMsgEncerraPrograma();
-				}
-			
-				JOptionPane.showConfirmDialog(null, "Movimentar portas ou janelas");
-			break;
-			
+				break;
+
 			case 2:
-				JOptionPane.showConfirmDialog(null, "Ver informações da casa");
-			break;
+				if (verificaConstrucaoCasa.equals("Contrução bem sucedida!")) {
+					// OT11.2
+					String informacoes = this.casa.geraInfoCasa();
+					// OT11.4
+					EntradaSaida.exibeInfoCasa(informacoes);
+
+					JOptionPane.showConfirmDialog(null, "Ver informações da casa");
+				} else {
+					JOptionPane.showMessageDialog(null, "Não há casa construida!");
+				}
+				break;
 			}
-		}while(opcao!=3);
-		
+		} while (opcao != 3);
+
 		EntradaSaida.exibeMsgEncerraPrograma();
-		
+
 		System.exit(0);
 	}
 }
--- WorkspacePOO_Mauesck/ConstrucaoCasa/src/visualizacao/EntradaSaida.java
+++ WorkspacePOO_Mauesck/ConstrucaoCasa/src/visualizacao/EntradaSaida.java
@@ -9,95 +9,111 @@ import modelo.Aberturas;
 
 public class EntradaSaida {
 
-	//OT8.3
+	// OT8.3
 	public static int solicitaOpcao() {
-		String[] opcoes = {"Construir casa","Movimentar portas ou janelas","Ver informacoes da casa","Sair do programa"};
+		String[] opcoes = { "Construir casa", "Movimentar portas ou janelas", "Ver informacoes da casa",
+				"Sair do programa" };
 		JComboBox<String> menu = new JComboBox<String>(opcoes);
-		JOptionPane.showConfirmDialog(null, menu, "Selecione a opcao desejada",JOptionPane.OK_CANCEL_OPTION);
+		JOptionPane.showConfirmDialog(null, menu, "Selecione a opcao desejada", JOptionPane.OK_CANCEL_OPTION);
 		return menu.getSelectedIndex();
 	}
-	
-	//OT8.4
+
+	// OT8.4
 	public static void exibeMsgEncerraPrograma() {
 		JOptionPane.showMessageDialog(null, "O programa será encerrado!");
 	}
-	
-	//OT9.5
+
+	// OT9.5
 	public static String solicitaDescricao(String descricao, int ordem) {
-		if(ordem==0) {
-			return JOptionPane.showInputDialog("Informe a descricao da "+descricao);
-		}else {
-			return JOptionPane.showInputDialog("Informe a descricao da "+ordem+" "+descricao);	
+		if (ordem == 0) {
+			return JOptionPane.showInputDialog("Informe a descricao da " + descricao);
+		} else {
+			return JOptionPane.showInputDialog("Informe a descricao da " + ordem + " " + descricao);
 		}
 	}
-	
+
 	public static String solicitaCor() {
 		return JOptionPane.showInputDialog("Informe a cor da casa");
 	}
-	
-	public static int solicitaQtdeAberturas(String abertura) {
-		return Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de "+abertura));
-	}
-	
-	//OT9.7
-	public static int solicitaEstado(String tipoAbertura) {
-		String[] opcoes = {"Fechada","Aberta","teste"};
-		
-		return JOptionPane.showOptionDialog(null, "Informe o estado da "+tipoAbertura, 
-				"Estado", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[1]);
-	}
-	//OT10.2
-	public static String solicitaTipoAbertura() {
-		//Vetor opcoes de 2 posições.
-		String[] opcoes = {"Porta","Janela"};
-		
-		//Var tipoAbertura recebendo a abertura a ser movida.
-		int tipoAbertura = JOptionPane.showOptionDialog(null, "Informe qual tipo de abertura deseja mover", 
-				"Mover abertura", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, 
-				null, opcoes, opcoes[0]);
 
-		//Validação para definir oque o metódo irá retornar. "opcoes" retorna "porta", "opcoes[0]" retorna "janela".
-		if(tipoAbertura==0) {
+	public static int solicitaQtdeAberturas(String abertura) {
+		int valor = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de " + abertura));
+		while (valor <= 0) {
+			JOptionPane.showMessageDialog(null, "Informe uma quantidade de "+abertura+" maior que 0!");
+			valor = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de " + abertura));
+		}
+		return valor;
+	}
+
+	// OT9.7
+	public static int solicitaEstado(String tipoAbertura) {
+		String[] opcoes = { "Fechada", "Aberta"};
+
+		return JOptionPane.showOptionDialog(null, "Informe o estado da " + tipoAbertura, "Estado",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[1]);
+	}
+
+	// OT10.2
+	public static String solicitaTipoAbertura() {
+		// Vetor opcoes de 2 posições.
+		String[] opcoes = { "Porta", "Janela" };
+
+		// Var tipoAbertura recebendo a abertura a ser movida.
+		int tipoAbertura = JOptionPane.showOptionDialog(null, "Informe qual tipo de abertura deseja mover",
+				"Mover abertura", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+
+		// Validação para definir oque o metódo irá retornar. "opcoes" retorna "porta",
+		// "opcoes[0]" retorna "janela".
+		if (tipoAbertura == 0) {
 			return "porta";
-		}else {
+		} else {
 			return "janela";
 		}
 	}
-	
-	//OT10.4
+
+	// OT10.4
 	public static int solicitaAberturaMover(ArrayList<Aberturas> listaDeAberturas) {
-		//Descobrindo se na posição há uma porta ou janela.
+		// Descobrindo se na posição há uma porta ou janela.
 		String tipoAbertura = listaDeAberturas.get(0).getClass().getName();
-		/*Acima o metodo getName() traz a ocorrencia completa da posição em listaDeAbertura, 
-		usamos então replaceAll para substituir "modelo." por "", ficando apenas "porta" ou 
-		"janela" e atribuindo a tipoAbertura*/ 
+		/*
+		 * Acima o metodo getName() traz a ocorrencia completa da posição em
+		 * listaDeAbertura, usamos então replaceAll para substituir "modelo." por "",
+		 * ficando apenas "porta" ou "janela" e atribuindo a tipoAbertura
+		 */
 		tipoAbertura = tipoAbertura.replaceAll("modelo.", "");
-		//Descobrindo quantos elemento a lista possui através de size().
+		// Descobrindo quantos elemento a lista possui através de size().
 		int qtdeAbertura = listaDeAberturas.size();
-		/*Vetor criado com a quantidade de elementos em listaDeAberturas, repare que o método
-		construtor que determina o tamanho do vetor é qtdeAbertura*/
+		/*
+		 * Vetor criado com a quantidade de elementos em listaDeAberturas, repare que o
+		 * método construtor que determina o tamanho do vetor é qtdeAbertura
+		 */
 		String[] descricoesAberturas = new String[qtdeAbertura];
-		//For que tera como base de repetição qtdeAbertura.
-		for(int i=0; i<qtdeAbertura; i++) {
-			descricoesAberturas[i]=listaDeAberturas.get(i).getDescricao();
+		// For que tera como base de repetição qtdeAbertura.
+		for (int i = 0; i < qtdeAbertura; i++) {
+			descricoesAberturas[i] = listaDeAberturas.get(i).getDescricao();
 		}
-		//msg recebe a porta ou janela a ser movimentada.
-		String msg = "Escolha a "+tipoAbertura+"a ser movimentada";
-		//Objeto exibicaoAberturas irá receber os dados de descricoesAberturas. 
+		// msg recebe a porta ou janela a ser movimentada.
+		String msg = "Escolha a " + tipoAbertura + "a ser movimentada";
+		// Objeto exibicaoAberturas irá receber os dados de descricoesAberturas.
 		JComboBox exibicaoAberturas = new JComboBox(descricoesAberturas);
-		int confirmacao = JOptionPane.showConfirmDialog(null, exibicaoAberturas, msg,
-				JOptionPane.OK_CANCEL_OPTION);
-		
-		if(confirmacao==0) {
+		int confirmacao = JOptionPane.showConfirmDialog(null, exibicaoAberturas, msg, JOptionPane.OK_CANCEL_OPTION);
+
+		if (confirmacao == 0) {
 			return exibicaoAberturas.getSelectedIndex();
-		}else {
+		} else {
 			return -1;
 		}
-		
+
 	}
-	
-	//OT10.7
+
+	// OT10.7
 	public static void exibeMsgAbertura() {
 		JOptionPane.showMessageDialog(null, "Nenhuma abertura será movimentada");
 	}
+
+	// OT11.3
+	public static void exibeInfoCasa(String informacoes) {
+		JOptionPane.showMessageDialog(null, informacoes, "Informações da casa", JOptionPane.INFORMATION_MESSAGE);
+	}
+
 }
